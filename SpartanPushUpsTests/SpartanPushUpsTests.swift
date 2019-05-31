07d8b4fca81f126c16b4baee41363e0da19d164a
Rijad Babovic <rijad.babovic@hotmail.com>
2019-05-31 11:33:34 +0200

    Initial Commit

---
+++ SpartanPushUpsTests/SpartanPushUpsTests.swift
@@ -0,0 +1,34 @@
+//
+//  SpartanPushUpsTests.swift
+//  SpartanPushUpsTests
+//
+//  Created by Rijad Babovic on 31/05/2019.
+//  Copyright © 2019 Rijad Babovic. All rights reserved.
+//
+
+import XCTest
+@testable import SpartanPushUps
+
+class SpartanPushUpsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
