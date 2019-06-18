be484a780043f0de436b63fab9853a06eedf1386
Rijad Babovic <rijad.babovic@hotmail.com>
2019-06-18 11:32:55 +0200

    first new commit

--- SpartanPushUps/AppDelegate.swift
+++ SpartanPushUps/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        application.statusBarStyle = .lightContent
+        //application.statusBarStyle = .lightContent
         
         self.window = UIWindow(frame: UIScreen.main.bounds)
         let mainController = MainViewController(nibName: nil, bundle: nil)
